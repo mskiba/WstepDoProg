6620448cb13864413413746c49f2e1dd8dfea2c4
Marek Skiba <skibamarek@gmail.com>
2010-10-30 20:06:32 +0200

    deleted bo tak

--- lab-01/2.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-main () {
- 	int n;
-    for (n=10; n<=50; n=n+1) {
-    	printf("%i. CZESC!\n", n);
-	}				       
-}
--- lab-01/3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-main () {
- 	int wynik=1,wykladnik=10,podstawa=2;
-	while (wykladnik > 0) {
-		wynik = wynik*podstawa;
-		wykladnik = wykladnik - 1;
-	}
-	printf("Wynik to: %i.\n",wynik);
-}
